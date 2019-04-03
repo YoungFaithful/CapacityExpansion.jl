4e53b7a56080e337da9e82e83e7e0332f8519add
elias.kuepper <elias.kuepper@rwth-aachen.de>
2019-04-02 22:46:56 -0700

    Docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(sitename="CEP.jl",
     pages = [
         "Introduction" => "index.md",
         "Workflow" => "workflow.md",
-        "Data Preparation" => ["preparing_clust_data.md", "preparing_opt_cep_data.md", "csv_structure.md", "README_GER_18", "README_GER_1", "README_CA_14", "README_CA_1", "README_TX_1"],
+        "Data Preparation" => ["preparing_clust_data.md", "preparing_opt_cep_data.md", "csv_structure.md", "README_GER_18.md", "README_GER_1.md", "README_CA_14.md", "README_CA_1.md", "README_TX_1.md"],
         "Optimization" => ["opt_cep.md","opt_cep_examples.md"],
         ],
     assets = [
