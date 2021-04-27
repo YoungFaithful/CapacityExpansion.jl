27dd491ab1e816e69c818b8777c2dc5da77887d8
YoungFaithful <elias.kuepper@rwth-aachen.de>
2021-04-27 09:58:56 +0200

    Jump (#65)
    * JuMP v0.20 update
    
    Co-authored-by: YoungFaithful <elias.kuepper@kerith.net>

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Clp
         ts_input_data = load_timeseries_data_provided("TX_1"; T=24, years=[2008])
         cep_data = load_cep_data_provided("TX_1")
         # run optimization
-        model = run_opt(ts_input_data,cep_data,Clp.Optimizer;optimizer_config=Dict{Symbol,Any}(:LogLevel => 0))
+        model = run_opt(ts_input_data,cep_data,Clp.Optimizer)
         # compare to exact result
         exact_res=[70540.26439790576;0.0;0.0;8498.278397905757;80132.88454450261;0.0]
         @test exact_res ≈ model.variables["CAP"].data[:,1,1] atol=1
@@ -24,7 +24,7 @@ using Clp
             ts_full_data = run_clust(ts_input_data;method="hierarchical",representation="centroid",n_init=1,n_clust=30)
             ## OPTIMIZATION ##
             optimizer=Clp.Optimizer
-            optimizer_config=Dict{Symbol,Any}(:LogLevel => 0)
+            optimizer_config=Dict{Symbol,Any}()
             scenarios["$state-$years-co2"] = run_opt(ts_clust_data.clust_data,cep_data,optimizer;descriptor="co2",limit_emission=Dict{String,Number}("CO2/electricity"=>1000),optimizer_config=optimizer_config)
             scenarios["$state-$years-slack"] = run_opt(ts_clust_data.clust_data,cep_data,optimizer;descriptor="slack",lost_load_cost=Dict{String,Number}("electricity"=>1e6), lost_emission_cost=Dict{String,Number}("CO2"=>700),optimizer_config=optimizer_config)
             scenarios["$state-$years-ex"] = run_opt(ts_clust_data.clust_data,cep_data,optimizer;descriptor="ex",infrastructure=Dict{String,Array}("existing"=>["all"]),optimizer_config=optimizer_config)
@@ -35,7 +35,7 @@ using Clp
         end
     end
     #Test transmission for a multi-node scenario
-    @testset "workflow $state" for (state, years) in [["GER_18", [2016]],["CA_14", [2016]]] begin
+    @testset "workflow $state" for (state, years) in [["CA_14", [2016]]] begin
            # laod data
            ts_input_data = load_timeseries_data_provided(state; T=24, years=years) #CEP
            cep_data = load_cep_data_provided(state)
@@ -43,7 +43,7 @@ using Clp
            ts_clust_data = run_clust(ts_input_data;method="hierarchical",representation="centroid",n_init=1,n_clust=3)
            ## OPTIMIZATION ##
            optimizer=Clp.Optimizer
-           scenarios["$state-$years-trans"] = run_opt(ts_clust_data.clust_data,cep_data,optimizer;descriptor="trans",transmission=true,optimizer_config=Dict{Symbol,Any}(:LogLevel => 0))
+           scenarios["$state-$years-trans"] = run_opt(ts_clust_data.clust_data,cep_data,optimizer;descriptor="trans",transmission=true,optimizer_config=Dict{Symbol,Any}())
        end
    end
    #Test exact values for each of the previously calculated scenarios by comparison with exact scenarios
@@ -51,7 +51,7 @@ using Clp
    @testset "exact $k" for (k,v) in scenarios begin
             #Test for each variable within this scenario
             @testset "variable $kv" for (kv,vv) in v.variables begin
-                    @test sum((round.(round.(exact_scenarios[k].variables[kv].data; digits=4); sigdigits=4).== round.(round.(vv.data;digits=4); sigdigits=4)).== false) == 0
+                    @test sum((round.(round.(exact_scenarios[k].variables[kv].data; digits=3); sigdigits=3).== round.(round.(vv.data;digits=3); sigdigits=3)).== false) == 0
                 end
             end
         end
